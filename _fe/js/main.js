c43d0a8a7fe3a5f9764a7d3a474cc5324d988ae8
Dale Karp <me@dale.io>
2013-08-13 10:21:53 -0400

    magic js goodness!

--- _fe/js/main.js
+++ _fe/js/main.js
@@ -2,11 +2,12 @@ requirejs.config({
   paths: {
     text: '../../bower_components/text/text',
     jquery: '../../bower_components/jquery/jquery',
+    jqueryui: '../../bower_components/jquery-ui/ui/jquery-ui',
     jade: '../../bower_components/jade/runtime'
   }
 });
 
-require(['jquery', 'templates', 'text!fake.json'], function ($, templates, tileData) {
+require(['jquery', 'templates', 'tiles', 'text!fake.json'], function ($, templates, tiles, tileData) {
   var tileData = JSON.parse(tileData)
 
   $('body').append(templates.header({
@@ -16,4 +17,6 @@ require(['jquery', 'templates', 'text!fake.json'], function ($, templates, tileD
   $('body').append(templates.tiles({
     tiles: tileData
   }));
+
+  tiles.init();
 });
