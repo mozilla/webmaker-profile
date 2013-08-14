eb4acad167bee95b43f268f04ced058b591d2989
Gvn Suntop <gavin@gsuntop.com>
2013-08-14 11:34:28 -0400

    hackable tile upgrade

--- _fe/js/hackable-tile.js
+++ _fe/js/hackable-tile.js
@@ -1,9 +1,9 @@
 define(['jquery', 'js/templates'], function ($, templates) {
 
   var HackableTile = function (target, options) {
-    var self = this,
-      defaults,
-      option;
+    var self = this;
+    var defaults;
+    var option;
 
     // Options ----------------------------------------------------------------
 
@@ -21,7 +21,8 @@ define(['jquery', 'js/templates'], function ($, templates) {
     self.$tileContent = $(templates.hackableTile());
     self.$textarea = self.$tileContent.filter('textarea');
     self.$hackedContent = self.$tileContent.filter('.hacked-content');
-    self.$hackButton = self.$tileContent.filter('button');
+    self.$hackButton = self.$tileContent.filter('.hack');
+    self.$saveButton = self.$tileContent.filter('.save');
 
     // Properties -------------------------------------------------------------
 
@@ -32,19 +33,33 @@ define(['jquery', 'js/templates'], function ($, templates) {
     // Event Delegation -------------------------------------------------------
 
     self.$textarea.on('blur', function (event) {
-      self.$hackedContent.html(self.$textarea.val());
+      self.pullText();
       self.$textarea.hide();
-      self.$hackedContent.show();
+      self.$hackButton.show();
+      self.$saveButton.hide();
+      self.$hackButton.fadeTo(400, 1);
     });
 
     self.$hackButton.on('click', function (event) {
       self.$textarea.show();
+      self.$hackButton.fadeTo(400, 0.5);
+      self.$saveButton.show();
       self.$textarea.focus();
     });
   };
 
   HackableTile.prototype = {
+    pullText: function () {
+      var self = this;
+      var textContent = self.$textarea.val();
 
+      if (textContent.length) {
+        self.$hackedContent.html(textContent);
+        self.$hackedContent.show();
+      } else {
+        self.$hackedContent.hide();
+      }
+    }
   };
 
   return HackableTile;
