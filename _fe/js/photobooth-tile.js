18afe1016bcc640974aab9a186eeade93fa9ae50
Kate Hudson <k88hudson@gmail.com>
2013-08-15 10:47:11 -0400

    fixups

--- _fe/js/photobooth-tile.js
+++ _fe/js/photobooth-tile.js
@@ -1,15 +1,15 @@
-define(['jquery', 'js/templates'], function ($, templates) {
+define(['jquery', 'templates', 'komponent'], function ($, templates) {
 
   navigator.getMedia = (navigator.getUserMedia ||
     navigator.webkitGetUserMedia ||
     navigator.mozGetUserMedia ||
     navigator.msGetUserMedia);
 
-
   var Photobooth = function (container) {
     var self = this;
-    var streaming = false;
 
+    self.callbacks = {};
+    self.streaming = false;
     self.video = container.querySelector('video');
     self.$video = $(self.video);
     self.$photo = $('img', container);
@@ -17,26 +17,46 @@ define(['jquery', 'js/templates'], function ($, templates) {
     self.$canvas = $(self.canvas);
     self.$startbtn = $('.trigger', container);
     self.width = $('.grid-sizer').width();
+
+  };
+
+  Photobooth.prototype = new Komponent();
+
+  Photobooth.prototype.init = function () {
+    var self = this;
+
     self.video.addEventListener('canplay', function () {
-      if (!streaming) {
+      if (!self.streaming) {
         self.height = self.video.videoHeight / (self.video.videoWidth / self.width);
-        self.video.setAttribute('width', self.width);
-        self.video.setAttribute('height', self.height);
-        self.canvas.setAttribute('width', self.width);
-        self.canvas.setAttribute('height', self.height);
+        self.$video.attr('width', self.width);
+        self.$video.attr('height', self.height);
+        self.$canvas.attr('width', self.width);
+        self.$canvas.attr('height', self.height);
         self.$startbtn.removeClass('hidden');
-        streaming = true;
+        self.streaming = true;
+        self.fire('resize');
       }
     }, false);
 
     function resetWidthHeight() {
-      var reset = {
+      self.$photo.css({
         width: '',
         height: ''
-      };
-      self.$photo.css(reset);
+      });
       self.$video.attr('height', '');
       self.$video.attr('width', '');
+    }
+
+    function startVideo(e) {
+      self.$video.attr('width', self.width);
+      self.$video.attr('height', self.height);
+      self.$canvas.attr('width', self.width);
+      self.$canvas.attr('height', self.height);
+      self.$photo.addClass('hidden');
+      self.$startbtn.removeClass('edit');
+      self.$startbtn.off('click', startVideo);
+      self.$startbtn.on('click', takePicture);
+      self.fire('resize');
     }
 
     function takePicture(err) {
@@ -53,33 +73,18 @@ define(['jquery', 'js/templates'], function ($, templates) {
       self.$startbtn.on('click', startVideo);
       self.$startbtn.off('click', takePicture);
       resetWidthHeight();
+      self.fire('resize');
     }
 
-    function startVideo(e) {
-      self.video.setAttribute('width', self.width);
-      self.video.setAttribute('height', self.height);
-      self.canvas.setAttribute('width', self.width);
-      self.canvas.setAttribute('height', self.height);
-      self.$photo.addClass('hidden');
-      self.$startbtn.removeClass('edit');
-      self.$startbtn.off('click', startVideo);
-      self.$startbtn.on('click', takePicture);
-    }
+    self.$startbtn.click(takePicture);
 
-   self.$startbtn.click(takePicture);
-
-  };
-
-
-  Photobooth.prototype.init = function() {
-    var self = this;
     navigator.getMedia({
         video: true,
         audio: false
       },
       function (stream) {
         if (navigator.mozGetUserMedia) {
-          video.mozSrcObject = stream;
+          self.video.mozSrcObject = stream;
         } else {
           var vendorURL = window.URL || window.webkitURL;
           self.video.src = vendorURL.createObjectURL(stream);
@@ -87,7 +92,7 @@ define(['jquery', 'js/templates'], function ($, templates) {
         self.video.play();
       },
       function (err) {
-        console.log('Error');
+        console.log('Error - getUserMedia not working');
       }
     );
   };
