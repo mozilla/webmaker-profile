f149e76ff7cb03d13411b90b2b47bccc5d6f36dd
Kate Hudson <k88hudson@gmail.com>
2013-08-28 16:47:00 -0400

    f

--- _fe/js/tiles.js
+++ _fe/js/tiles.js
@@ -237,7 +237,7 @@ define([
 
       // Run packery layout after all images have loaded
       imagesLoaded(self.container, function () {
-        //$('.loader').hide();
+        $('.loader').hide();
         self.packery.layout();
       });
     },
