cbeb074fc4a21f891fdeb5c1df359dce00e2338e
Kate Hudson <k88hudson@gmail.com>
2013-08-29 21:59:35 -0400

    shared streams = done, gifs = yay, still need to do update function

--- _fe/js/main.js
+++ _fe/js/main.js
@@ -1,6 +1,8 @@
 requirejs.config({
   baseUrl: '../bower_components',
   paths: {
+    animatedGif: 'Animated_GIF/src/Animated_GIF',
+    getUserMedia: 'getUserMedia/getusermedia.bundle',
     imagesloaded: 'imagesloaded/imagesloaded',
     jade: 'jade/runtime',
     jquery: 'jquery/jquery',
@@ -10,6 +12,8 @@ requirejs.config({
     komponent: 'komponent/komponent',
     lodash: 'lodash/lodash',
     main: '../_fe/js/main',
+    neuquant: 'Animated_GIF/src/NeuQuant',
+    omggif: 'Animated_GIF/src/omggif',
     store: 'store.js/store',
     uuid: 'node-uuid/uuid',
     templates: '../_fe/compiled/jade-templates',
--- _fe/js/photobooth-tile.js
+++ _fe/js/photobooth-tile.js
@@ -1,9 +1,20 @@
-define(['jquery', 'templates', 'js/tile'], function ($, templates, Tile) {
+define([
+  'jquery',
+  'templates',
+  'js/tile',
+  'animatedGif',
+  'getUserMedia'
+], function (
+  $,
+  templates,
+  Tile,
+  AnimedGif,
+  getUserMedia
+) {
 
-  navigator.getMedia = (navigator.getUserMedia ||
-    navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia ||
-    navigator.msGetUserMedia);
+  var COUNT = 5;
+  var DELAY = 200;
+  var STREAM;
 
   var Photobooth = function (container) {
     var self = this;
@@ -12,6 +23,7 @@ define(['jquery', 'templates', 'js/tile'], function ($, templates, Tile) {
 
     // Element references -----------------------------------------------------
 
+    self.$container = $(container);
     self.$video = $('video', container);
     self.$photo = $('img', container);
     self.$canvas = $('canvas', container);
@@ -24,98 +36,116 @@ define(['jquery', 'templates', 'js/tile'], function ($, templates, Tile) {
     self.bindCommonUI(container);
 
     // Properties -------------------------------------------------------------
+    self.width = 0;
+    self.height = 0;
+    self.frames = [];
 
-    self.streaming = false;
-    self.width = $('.grid-sizer').width();
   };
 
   Photobooth.prototype = new Tile();
 
+  Photobooth.prototype.onErr = function(err) {
+    var self = this;
+    if (err.PERMISSION_DENIED) {
+      console.err('Looks like you denied us permission to use your camera :(');
+    } else if (err.NOT_SUPPORTED) {
+      // It's fine, just show the videoInput
+    }
+  };
+
+  Photobooth.prototype.makeGif = function (callback) {
+    var self = this;
+    var ag = new Animated_GIF({
+      workerPath: '/bower_components/Animated_GIF/src/quantizer.js'
+    });
+    ag.setSize(320, 240);
+    ag.setDelay(10);
+    for (var i = 0; i < self.frames.length; i++) {
+      ag.addFrame(self.frames[i]);
+    }
+    ag.getBase64GIF(function (image) {
+      var animatedImage = document.createElement('img');
+      animatedImage.src = image;
+      callback(animatedImage);
+    });
+  };
+
+
+  Photobooth.prototype.clickListener = function(onOff) {
+    var self = this;
+    var interval;
+    var count = 0;
+
+    function snapPicture() {
+      var img = document.createElement('img');
+      self.clickListener('off');
+      self.$canvas[0].getContext('2d').drawImage(self.$video[0], 0, 0, self.width, self.height);
+      img.src = self.$canvas[0].toDataURL('image/gif');
+      self.frames.push(img);
+      count++;
+      if (count === COUNT) {
+        clearInterval(interval);
+        self.makeGif(function(gif) {
+          gif.classList.add('output-img');
+          self.$container.append(gif);
+        });
+        count = 0;
+        self.clickListener('on');
+      }
+    }
+
+    function onClick() {
+      self.width = self.$video.width();
+      self.height = self.$video[0].videoHeight / (self.$video[0].videoWidth / self.width);
+
+      self.$video.attr('width', self.width);
+      self.$video.attr('height', self.height);
+      self.$canvas.attr('width', self.width);
+      self.$canvas.attr('height', self.height);
+
+      interval = setInterval(snapPicture, DELAY);
+
+      self.$video.removeAttr('width');
+      self.$video.removeAttr('height');
+    }
+
+    self.$video[onOff]('click', onClick);
+  }
+
+  Photobooth.prototype.onStreamLoaded = function (stream) {
+    var self = this;
+    if ( stream ) {
+      stream = window.URL.createObjectURL(stream);
+      STREAM = stream;
+    }
+
+    self.width = self.$video.width();
+    self.height = self.$video[0].videoHeight / (self.$video[0].videoWidth / self.width);
+
+    self.$video.attr('width', self.width);
+    self.$video.attr('height', self.height);
+    self.$video[0].src = STREAM;
+    self.$video[0].play();
+    self.clickListener('on');
+  }
+
   Photobooth.prototype.init = function () {
     var self = this;
 
-    function resetWidthHeight() {
-      self.$photo.css({
-        width: '',
-        height: ''
+    if ( STREAM ) {
+      self.onStreamLoaded();
+    } else {
+      getUserMedia(function (err, stream) {
+          // if the browser doesn't support user media
+          // or the user says "no" the error gets passed
+          // as the first argument.
+          if (err) {
+            self.onErr(err);
+          } else {
+            self.onStreamLoaded(stream);
+          }
       });
-
-      self.$video.attr('height', '');
-      self.$video.attr('width', '');
     }
-
-    function startVideo(e) {
-      e.stopPropagation();
-      self.$video.attr('width', '100%');
-      self.$video.attr('height', self.width / self.height);
-      self.$canvas.attr('width', '100%');
-      self.$canvas.attr('height', self.width / self.height);
-      self.$photo.addClass('hidden');
-      self.$startbtn.removeClass('edit');
-      self.$startbtn.off('click', startVideo);
-      self.$startbtn.on('click', takePicture);
-      self.fire('resize');
-    }
-
-    function takePicture(e) {
-      e.stopPropagation();
-      var data;
-      self.height = self.$video[0].videoHeight / (self.$video[0].videoWidth / self.width);
-      self.$canvas.width(self.width);
-      self.$canvas.height(self.height);
-      self.$canvas[0].getContext('2d').drawImage(self.$video[0], 0, 0, self.width, self.height);
-      data = self.$canvas[0].toDataURL('image/png');
-      self.$photo.attr('src', data);
-      self.$photo.removeClass('hidden');
-
-      self.$startbtn.addClass('edit');
-      self.$startbtn.on('click', startVideo);
-      self.$startbtn.off('click', takePicture);
-      resetWidthHeight();
-      self.fire('resize');
-    }
-
-    function onTimeUpdate() {
-      if (!self.$video[0].videoHeight) {
-        return;
-      }
-      if (!self.streaming) {
-        self.height = self.$video[0].videoHeight / (self.$video[0].videoWidth / self.width);
-        self.$video.attr('width', self.width);
-        self.$video.attr('height', self.height);
-        self.$canvas.attr('width', self.width);
-        self.$canvas.attr('height', self.height);
-        self.$startbtn.removeClass('hidden');
-        self.streaming = true;
-        self.fire('resize');
-      }
-      self.$video.off('timeupdate', onTimeUpdate);
-    }
-    self.$video.on('timeupdate', onTimeUpdate);
-    self.$startbtn.click(takePicture);
-
-    navigator.getMedia({
-        video: true
-      },
-      function (stream) {
-        self.stream = window.URL.createObjectURL(stream);
-        self.$video[0].src = self.stream;
-        self.$video[0].play();
-        self.$statusMessage.empty();
-      },
-      function (err) {
-        // TODO - Move these strings in to Require-able messages.json
-        var message = 'Oops, there was an error accessing your webcam';
-
-        if (err.PERMISSION_DENIED) {
-          message = 'Looks like you denied us permission to use your camera :(';
-        } else if (err.NOT_SUPPORTED) {
-          message = 'Oops, your browser doesn\'t support access to your webcam';
-        }
-
-        self.$statusMessage.html(message);
-      }
-    );
   };
 
   Photobooth.prototype.update = function () {
