f58a665d9e35c216820d3f4bdcb4e3dbaa3de94b
Jon Buckley <jon@jbuckley.ca>
2013-10-17 15:40:57 -0400

    Hack to work around Gruntfile & cwd() dependency

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-var grunt = require('grunt');
-require('./Gruntfile')(grunt);
-
 module.exports = {
   build: function build(cb) {
+    var cwd = process.cwd();
+    process.chdir(__dirname);
+
+    var grunt = require('grunt');
+    require('./Gruntfile')(grunt);
+
     grunt.tasks(['build'], { force: true }, function() {
-      cb();
+      process.chdir(cwd);
+
+      if (cb) {
+        cb();
+      }
     });
   }
 };
